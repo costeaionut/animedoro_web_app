9860e9c75bdfddee0669185f42eda6db453110cc
Anca Ivancescu <anca.ivancescu@customsoft.ro>
2021-04-07 19:07:49 +0300

    mainPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,72 @@
 import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import {Navbar, Nav, NavDropdown, Button} from 'react-bootstrap'
 
-function App() {
-  return (
-    <div className="App">
-      <div className="App-content">
-      <Navbar >
-  <Navbar.Brand href="#home"></Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">bunton 1</Nav.Link>
-      <Nav.Link href="#link">buton 2</Nav.Link>
-      <NavDropdown title="MENU" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
-<div className="Cronometru">
-  Content
-</div>
+import Home from './pages/Home';
+import Cronometru from './pages/Cronometru';
+import Login from './pages/Login';
 
-    </div>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props) {
+    /*
+      oricand se schimba state.pagina_curenta, afisam altceva in Render()
+
+      pagini:
+        - home
+        - register
+        - cronometru
+
+
+        // dupa login cu succes, this.setState({pagina_curenta: 'cronometru' })
+    */
+    super(props);
+    this.state = {
+      pagina_curenta: 'home',
+      plus: false,
+      minus: false
+    }
+
+    // setTimeout(() => this.setState({pagina_curenta: 'cronometru' }), 5000)
+
+  }
+
+  render(){
+
+    let paginaRandata = (<Home/>);
+    if (this.state.pagina_curenta === 'login') {
+      paginaRandata = (<Login trimisVariabilaPrinParametru={'variabila trimisa ca paramentru'}/>);
+    }
+    if (this.state.pagina_curenta === 'cronometru') {
+      paginaRandata = (<Cronometru/>);
+    }
+
+    return (
+      <div className="App">
+        <div className="Navbar-ceva">
+        <div className="Navbar-content">
+        {/* {this.state.pagina_curenta} */}
+        <Navbar >
+          <Navbar.Brand href="#home"></Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+          <Nav.Link onClick={() => this.setState({pagina_curenta: 'home'})}>Home</Nav.Link>
+          <Nav.Link onClick={() => this.setState({pagina_curenta: 'login'})}>Login</Nav.Link>
+          {/* <Nav.Link onClick={() => this.setState({pagina_curenta: 'cronometru'})}>Cronometru</Nav.Link> */}
+        </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        </div>
+      </div>
+      <div className="Cronometru">
+        {paginaRandata}
+      </div>
+
+      </div>
+      
+    );
 }
-
+}
 export default App;
+
